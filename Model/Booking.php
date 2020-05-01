546e5b35ffd00ea17e5c716d8be69ccb0503c185
mehdijalali20 <63256609+mehdijalali20@users.noreply.github.com>
2020-05-01 04:22:33 -0700

    Create Booking.php

---
+++ Model/Booking.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Booking extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [
+    	'room_id',
+    	'start',
+    	'end',
+    	'is_reservation',
+    	'is_paid',
+    	'notes',
+    ];
+
+    public function room()
+    {
+    	return $this->belongsTo('App\Room');
+    }
+
+    public function users()
+    {
+    	return $this->belongsToMany('App\User', 'bookings_users', 'booking_id', 'user_id')->withTimestamps();
+    }
+}
