cee56868d5d2d59386076157cb922e9757f6197e
mehdijalali20 <63256609+mehdijalali20@users.noreply.github.com>
2020-05-01 03:44:32 -0700

    Create show.blade.php

---
+++ views/bookings/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <dd>{{ $booking->id }}</dd>
+    <dd>{{ $booking->room_id }}</dd>
+    <dd>{{ date('F d, Y', strtotime($booking->start)) }}</dd>
+    <dd>{{ date('F d, Y', strtotime($booking->end)) }}</dd>
+    <dd>{{ $booking->is_reservation ? 'Yes' : 'No' }}</dd>
+    <dd>{{ $booking->is_paid ? 'Yes' : 'No' }}</dd>
+    <dd>{{ $booking->room_id }}</dd>
+    <dd>{{ date('F d, Y', strtotime($booking->created_at)) }}</dd>
+    <dd>{{ date('F d, Y', strtotime($booking->updated_at)) }}</dd>
+    @foreach($booking->users as $user)
+        <p>{{ $user->name }}</p>
+    @endforeach
