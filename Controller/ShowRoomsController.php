5d97c5ecb57f8f2e3243f220ef0e875e23352e89
mehdijalali20 <63256609+mehdijalali20@users.noreply.github.com>
2020-05-01 04:08:10 -0700

    Update ShowRoomsController.php

--- Controller/ShowRoomsController.php
+++ Controller/ShowRoomsController.php
@@ -1,1 +1,8 @@
+use App\Room;
+use Illuminate\Support\Facades\DB;
 
+public function __invoke(Request $request, $roomType = null)
+{
+  $rooms = Room::byType($roomType)->get();
+  return view('rooms.index', ['rooms' => $rooms]);
+}
